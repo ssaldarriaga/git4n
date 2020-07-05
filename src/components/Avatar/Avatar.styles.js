6766ccd7f6cab01ba5c317584eda70de3e4f9785
Sebastian Saldarriaga <sebastian@ubidots.com>
2020-07-04 22:16:36 -0500

    Added dynamic size to avatar component

--- src/components/Avatar/Avatar.styles.js
+++ src/components/Avatar/Avatar.styles.js
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const Img = styled.img`
   border-radius: 50%;
-  height: 50px;
-  width: 50px;
+  height: ${({ height }) => height};
+  width: ${({ width }) => width};
 `;
--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -4,11 +4,14 @@ import PropTypes from 'prop-types';
 // Styled components
 import { Img } from './Avatar.styles';
 
-export function Avatar({ src, alt }) {
-  return <Img src={src} alt={alt} />
+export function Avatar({ src, alt, height = 50, width = 50, className }) {
+  return <Img src={src} alt={alt} height={height} width={width} className={className} />
 }
 
 Avatar.propTypes = {
-  src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  className: PropTypes.string,
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
